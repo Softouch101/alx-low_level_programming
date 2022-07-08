889200a86db790572d4eb63b79f9ba0ba8095337
Kingsley Oweku <oghenevogaga.oweku@gmail.com>
2022-07-08 08:20:00 -0700

    C-funs

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
+
 /**
- *   main - Entry point
- *   Return: Always 0 (success)
- */
+ *  main - Enty point  
+ *  Return: Always 0 (Success)
+*/
 int main(void)
 {
-	        int i;
-		long int l;
-	        long long int ll;      
-		float d;
-	        char c;
-		
-		printf("Size of a char: %lu byte(s)\n", sizeof(c));
-		printf("Size of an int: %lu byte(s)\n", sizeof(i));
-		printf("Size of a long int: %lu byte(s)\n", sizeof(l));
-		printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
-		printf("Size of a float: %lu byte(s)\n", sizeof(d));
-	        return (0);
+char acharacter;
+int ainteger;
+long along;
+long long alonglong;
+float afloat;
+printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
+printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));      					     printf("Size of a long int: %lu byte(s)\n", sizeof(along));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
+printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
+return (0);
 }
-
